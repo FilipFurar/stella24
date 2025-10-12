61750e9ccb9dc8bbf36dd8249a630e4cad033213
Filip Furár <furar@stud.uniza.sk>
2025-10-12 18:50:56 +0200

    add items

--- src/app.rs
+++ src/app.rs
@@ -1,57 +1,47 @@
 use eframe::epaint::Color32;
 use gethostname::gethostname;
 
-// Define color constants for UI elements
 const GREEN: Color32 = Color32::from_rgb(66, 170, 125);
 const BLUE: Color32 = Color32::from_rgb(75, 67, 185);
 const PINK: Color32 = Color32::from_rgb(194, 73, 125);
 
-
-// Main application struct
-#[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // Provides default values for fields
-pub struct AppStella {
-
+enum ItemKind {
+    Table,
+    Domain,
+    Connector,
 }
 
-// Default implementation for AppStella
+pub struct AppStella {
+    items: Vec<ItemKind>,
+}
+
 impl Default for AppStella {
     fn default() -> Self {
         Self {
-
+            items: Vec::new(),
         }
     }
 }
 
-// Main application logic
 impl AppStella {
-    // Create a new AppStella instance, restoring from storage if available
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        if let Some(storage) = cc.storage {
-            let app: Self = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-            app
-        } else {
-            Default::default()
-        }
+        Default::default()
     }
 }
 
-// Implementation of the eframe application
 impl eframe::App for AppStella {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Top menu panel for File operations
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
                 let is_web = cfg!(target_arch = "wasm32");
                 ui.menu_button("File", |ui| {
                     if !is_web && ui.button("Quit").clicked() {
                         ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        // Close the app
                     }
                 });
 
                 ui.separator();
-                egui::widgets::global_theme_preference_buttons(ui); // Add UI theme preferences
+                egui::widgets::global_theme_preference_buttons(ui);
 
                 ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
                     let text = "Welcome, ".to_string() + &gethostname().to_string_lossy();
@@ -60,7 +50,6 @@ impl eframe::App for AppStella {
             });
         });
 
-        // Workbench menu for adding items
         egui::TopBottomPanel::top("workbenchmenu_panel").show(ctx, |ui| {
             ui.add_space(3.0);
             egui::menu::bar(ui, |ui| {
@@ -68,34 +57,34 @@ impl eframe::App for AppStella {
                     .add(egui::Button::new("Table").stroke(egui::Stroke::new(1.0, BLUE)))
                     .clicked()
                 {
+                    self.items.push(ItemKind::Table);
                 }
                 if ui
                     .add(egui::Button::new("Domain").stroke(egui::Stroke::new(1.0, GREEN)))
                     .clicked()
                 {
+                    self.items.push(ItemKind::Domain);
                 }
                 if ui
                     .add(egui::Button::new("Connector").stroke(egui::Stroke::new(1.0, PINK)))
                     .clicked()
                 {
+                    self.items.push(ItemKind::Connector);
+
                 }
             });
             ui.add_space(2.0);
         });
 
-        // Central panel for displaying workbench items
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Workbench");
-
+            for item in &self.items {
+                ui.label("kkt");
+            }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                egui::warn_if_debug_build(ui); // Display debug warnings if applicable
+                egui::warn_if_debug_build(ui);
             });
         });
-    }
-
-    // Save the current state to storage
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
     }
 }
