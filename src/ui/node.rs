4bc1f2eaeb845e185c55b1922541255b09de73e2
Filip Furár <furar@stud.uniza.sk>
2026-02-27 14:29:31 +0100

    rustfmt

--- src/ui/node.rs
+++ src/ui/node.rs
@@ -66,24 +66,23 @@ impl Table {
 
 impl Domain {
     pub(crate) fn draw(&mut self, ui: &mut Ui, id: usize) {
-
-            ui.text_edit_singleline(&mut self.name);
-            ui.horizontal(|ui| {
-                ui.label("Type:");
-                let selected_text = DATA_TYPES[self.data_type.base].name.to_string();
-                egui::ComboBox::from_id_salt(format!("type_{id}"))
-                    .selected_text(selected_text)
-                    .show_ui(ui, |ui| {
-                        for (i, def) in DATA_TYPES.iter().enumerate() {
-                            if ui.selectable_label(false, def.name).clicked() {
-                                self.data_type = DataType {
-                                    base: i,
-                                    params: vec![0; def.param_count],
-                                }
+        ui.text_edit_singleline(&mut self.name);
+        ui.horizontal(|ui| {
+            ui.label("Type:");
+            let selected_text = DATA_TYPES[self.data_type.base].name.to_string();
+            egui::ComboBox::from_id_salt(format!("type_{id}"))
+                .selected_text(selected_text)
+                .show_ui(ui, |ui| {
+                    for (i, def) in DATA_TYPES.iter().enumerate() {
+                        if ui.selectable_label(false, def.name).clicked() {
+                            self.data_type = DataType {
+                                base: i,
+                                params: vec![0; def.param_count],
                             }
                         }
-                    });
-            });
+                    }
+                });
+        });
     }
 }
 
